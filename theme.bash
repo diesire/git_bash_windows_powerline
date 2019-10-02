01332664d6db477c5499cd16a47a965a2ad61be5
Stuart Axon <stuaxo2@yahoo.com>
2019-10-02 12:41:06 +0100

    Update theme.bash
    Speed up last_status further by not using $() around job collection.

--- theme.bash
+++ theme.bash
@@ -194,10 +194,12 @@ function __powerline_left_segment {
 }
 
 function __powerline_last_status_prompt {
-  local symbols=()
+  local symbols=()  
+  local stopped_jobs
+  jobs -sp | read -n1 stopped_jobs
   [[ $last_status -ne 0 ]] && symbols+="$(__color ${STATUS_PROMPT_ERROR_COLOR})${STATUS_PROMPT_ERROR}"
   [[ $UID -eq 0 ]] && symbols+="$(__color ${STATUS_PROMPT_ROOT_COLOR})${STATUS_PROMPT_ROOT}"
-  [[ ! -z $(read -r -n1 stopped_jobs <<< $(jobs -sp); echo $stopped_jobs) ]] && symbols+="$(__color ${STATUS_PROMPT_JOBS_COLOR})${STATUS_PROMPT_JOBS}"
+  [[ ! -z "$stopped_jobs" ]] && symbols+="$(__color ${STATUS_PROMPT_JOBS_COLOR})${STATUS_PROMPT_JOBS}"
 
   [[ -n "$symbols" ]] && echo "$symbols|${STATUS_PROMPT_COLOR}"
 }
